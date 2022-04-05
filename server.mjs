01ccb9fb72b6cbeb9f5bd8a72401cb3599bd9595
Matheus Fenolio do Prado <matheusfenoliodoprado@outlook.com>
2022-04-05 20:51:58 -0300

    Test

--- server.mjs
+++ server.mjs
@@ -2,6 +2,6 @@ import express from 'express';
 
 const server = express();
 
-server.get('', (req, res) => res.status(200).json('Hello World!'));
+server.get('', (req, res) => res.status(200).json('Hello Coolify!'));
 
 server.listen(9100, () => console.info('Server is running!'));
